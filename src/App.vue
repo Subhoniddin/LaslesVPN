f79d436c10022c120c4fa7071f41c1685e5f1dc2
Subhoniddin <subhoniddinodinaev@gmail.com>
2022-08-17 13:26:30 +0500

    adding task to repository

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,372 @@
-<template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+<template lang="">
+  <div class="container mx-auto">
+    <nav class='flex justify-between pt-6 w-4/5 mx-auto items-center'>
+    <div class="flex ">
+        <img src="./assets/Vector.png" alt="">
+         <p class="font-bold text-xl m-2 ">
+         LaslesVPN
+        </p>
+      </div>
+      <div class='flex justify-between w-96 items-center'>
+      <div>About</div>
+      <div>Fitures</div>
+      <div>Pricing</div>
+      <div>Testiomonials</div>
+      <div>Help </div>
+      </div>
+      <div>
+      <div class="flex justify-between w-40">
+      <button>Sign In</button>
+      <button class="box-border last:w-24 h-8 border border-3 border-red-500 rounded-2xl ">
+        <p class="text-red-500">Sign Up</p>
+        </button>
+      </div>
+      </div>
+    </nav>
+    <main>
+        <div class="flex justify-between w-4/5 mx-auto h-96 items-center">
+            <div>
+                <h1 class="not-italic text-4xl font-extrabold w-4/5">Want anything to be easy with LaslesVPN.</h1>
+               <p clasd="text-xs">Provide a network for all your needs with ease and fun using LaslesVPN discover interesting features from us.</p>
+               <button class="w-60 h-14 mt-7 shadow-xl shadow-red-300  bg-red-600 border border-3 rounded-2xl text-white">Get Started</button>
+            </div>
+            <div class="w-auto pt-10">
+                <img src='./assets/mainLogo.png' alt="">
+            </div>
+        </div>
+    </main>
+    <div class="w-4/5 flex justify-around mx-auto pt-10 shadow-xl">
+        <div class="flex items-center border-r-2 w-56">
+            <img class="w-14 h-12" src="./assets/user.png" alt="userlogo">
+            <div class="flex-col m-5">
+            <div>90+</div>
+            <div>Users</div>
+            </div>
+        </div>
+        <div class="flex items-center border-r-2 w-56">
+            <img class="w-14 h-14" src="./assets/location.png" alt="locationlogo">
+            <div class="flex-col m-5">
+            <div>30+</div>
+            <div>Location</div>
+            </div>
+        </div>
+        <div class="flex items-center">
+            <img class="w-14" src="./assets/server.png" alt="serverlogo">
+            <div class="flex-col m-5">
+            <div>50+</div>
+            <div>Servers</div>
+            </div>
+        </div>
+    </div>
+    <div class="w4/5 flex justify-between mx-auto p-24 items-center">
+            <div>
+                <img src="./assets/main_picture.png" alt="">
+            </div>
+            <div class="w-96">
+                <h1 class='text-4xl'>We Provide Many Features You Can Use</h1>
+                <h2 class="pt-8">You can explore the features that we provide with fun and have their own functions each feature.</h2>
+                <div class="flex items-center pt-8">
+                    <img class="w-10" src="./assets/tick.png" alt="" />
+                    <p>Powerfull online protection.</p>
+                </div>
+                <div class="flex items-center">
+                    <img class="w-10" src="./assets/tick.png" alt="" />
+                    <p>Internet without borders.</p>
+                </div>
+                <div class="flex items-center">
+                    <img class="w-10" src="./assets/tick.png" alt="" />
+                    <p>Supercharged VPN</p>
+                </div>
+                <div class="flex items-center">
+                    <img class="w-10" src="./assets/tick.png" alt="" />
+                    <p>No specific time limits.</p>
+                </div>
+            </div>
+        </div>
+        <div>
+            <div class="flex justify-center items-center ">
+                <h1 class="font-bold text-4xl">Choose Your Plan</h1>
+            </div>
+            <div class="flex justify-center">
+                <p>Let's choose the package that is best for you and explore it happily and cheerfully.</p>
+            </div>
+         </div>
+         <div class="flex w-4/5 justify-between mx-auto pt-14 pb-10">
+            <div class="flex-col justify-center items-stretch  w-80 h-full border-2 border-solid rounded-xl">
+               <div class="flex-col justify-between items-center">
+                   <img class="w-full" src="./assets/salebox.png" alt=""/>
+                   <div class="flex w-full justify-center items-center pb-3 font-medium text-xl">
+                    Free Plan
+                </div>
+                <div class="flex justify-center items-center m-2 mr-6">
+                    <img class="w-10" src="./assets/tick_outline.png" alt=""/>
+                    Unlimited Bandwitch
+                </div>
+                <div class="flex justify-center items-center m-2 mr-4">
+                    <img class="w-10" src="./assets/tick_outline.png" alt=""/>
+                    Encrypted Connection
+                </div>
+                <div class="flex justify-center items-center m-2 mr-6">
+                    <img class="w-10" src="./assets/tick_outline.png" alt=""/>
+                    Works on All Devices
+                </div>
+                <div class="flex justify-center items-center mr-10">
+                    <img class="w-10" src="./assets/tick_outline.png" alt=""/>
+                    Connect Anyware
+                </div>
+                <div class="flex justify-center font-medium text-xl pt-20">
+                    <h1>Free</h1>
+                </div>
+                <div class="flex justify-center pt-6 pb-10">
+                    <button class="box-border w-40 h-8 text-red-700 border border-3 border-red-500 rounded-2xl ">Select</button>
+                </div>
+               </div>
+            </div>
+            <div class="w-80 h-full flex-col justify-items-center border-2 border-solid rounded-xl">
+                 <img class="w-full" src="./assets/salebox.png" alt=""/>
+                 <div class="flex justify-center pb-3 font-medium text-xl">
+                     Standard Plan
+                 </div>
+                <div class="flex justify-center items-center m-2 mr-4">
+                    <img class="w-10" src="./assets/tick_outline.png" alt=""/>
+                    Unlimited Bandwitch
+                </div>
+                <div class="flex justify-center items-center m-2">
+                    <img class="w-10" src="./assets/tick_outline.png" alt=""/>
+                    Encrypted Connection
+                </div>
+                <div class="flex justify-center items-center m-2 mr-4">
+                    <img class="w-10" src="./assets/tick_outline.png" alt=""/>
+                    Works on All Devices
+                </div>
+                <div class="flex justify-center items-center mr-7">
+                    <img class="w-10" src="./assets/tick_outline.png" alt=""/>
+                    Connect Anyware
+                </div>
+                <div class="flex justify-center font-medium text-xl pt-20">
+                    <h1>Free</h1>
+                </div>
+                <div class="flex justify-center pt-6 pb-10">
+                    <button class="box-border w-40 h-8 text-red-700 border border-3 border-red-500 rounded-2xl ">Select</button>
+                </div>
+            </div>
+            <div class="w-80 h-full flex-col justify-items-center border-2 border-solid rounded-xl">
+                 <img class="w-full" src="./assets/salebox.png" alt=""/>
+                 <div class="flex justify-center pb-3 font-medium text-xl">
+                     Premium Plan
+                 </div>
+                <div class="flex justify-center items-center m-2 mr-4">
+                    <img class="w-10" src="./assets/tick_outline.png" alt=""/>
+                    Unlimited Bandwitch
+                </div>
+                <div class="flex justify-center items-center m-2">
+                    <img class="w-10" src="./assets/tick_outline.png" alt=""/>
+                    Encrypted Connection
+                </div>
+                <div class="flex justify-center items-center m-2 mr-4">
+                    <img class="w-10" src="./assets/tick_outline.png" alt=""/>
+                    Works on All Devices
+                </div>
+                <div class="flex justify-center items-center m-2 mr-10">
+                    <img class="w-10" src="./assets/tick_outline.png" alt=""/>
+                    Connect Anyware
+                </div>
+                <div class="flex justify-center items-center mr-7">
+                    <img class="w-10" src="./assets/tick_outline.png" alt=""/>
+                    Get New Features
+                </div>
+               <div class="flex justify-center font-medium text-xl pt-10">
+                    <h1>Free</h1>
+                </div>
+                <div class="flex justify-center pt-6 pb-10">
+                    <button class="box-border w-40 h-8  bg-red-600 shadow-xl shadow-red-300 text-white border border-3 border-red-500 rounded-2xl ">Select</button>
+                </div>
+            </div>
+         </div>
+         <div class="flex w-4/5 mx-auto justify-center">
+            <div class="flex w-96 font-medium text-4xl text-center">
+                <h1>Huge Global Network of Fast VPN</h1>
+            </div>
+         </div>
+         <div class="flex w-4/5 mx-auto justify-center pt-6 pb-4">
+            <div class="w-1/2 text-center">
+                <p>See LaslesVPN everywhere to make it easier for you when you move locations.</p>
+            </div>
+         </div>
+         <div class="w-4/5 mx-auto flex">
+             <img class="w-full" src="./assets/map.png" alt=""/>
+         </div>
+          <div class="flex justify-between w-4/5 mx-auto">
+                <div>
+                    <img src="./assets/netflix.png" alt="">
+                </div>
+                <div>
+                    <img src="./assets/reddit.png" alt="">
+                </div>
+                <div>
+                    <img src="./assets/amazon.png" alt="">
+                </div>
+                <div>
+                    <img src="./assets/discord.png" alt="">
+                </div>
+                <div>
+                    <img src="./assets/spotify.png" alt="">
+                </div>
+            </div>
+            <div class="flex w-4/5 mx-auto justify-center pt-10">
+            <div class="flex w-96 font-medium text-4xl text-center">
+                <h1>Trusted by Thousands of Happy Customer</h1>
+            </div>
+         </div>
+         <div class="flex w-4/5 mx-auto justify-center pt-6 pb-20">
+            <div class="w-2/4 text-center">
+                <p>These are the stories of our customers who have joined us with great pleasure when using this crazy feature.</p>
+            </div>
+         </div>
+         <div class="flex flex-row w-4/5 mx-auto">
+            <div class="flex flex-row w-full justify-between items-center">
+                <swiper
+                :modules="modules"
+                :loop="true"
+                :pagination="{clickable: true}"
+                :autoplay="{
+                    delay: 7000,
+                    disableOnInteraction: false,
+                    pauseOnMouseEnter: true
+                }"
+                >
+                    <swiper-slide v-for="text in swiperTextBase" :key="text.title">
+                    <div class="rev_slider w-[400px] h-60 border border-3 border-red-500 rounded-2xl mr-2">
+                        <div class="slide">
+                            <div class="avatar w-[400px] flex justify-between items-center p-2 ">
+                                <img :src="text.img" alt="image">
+                            <div class="name">
+                                <h1 class="font-medium text-xl">{{text.userName}}</h1> 
+                                <p class="text-lg">{{text.country}}</p>
+                            </div>
+                            <div class="flex m-1">
+                                4.5 
+                                <img class="m-1" src="./assets/star.png" alt="">
+                                </div>
+                            </div>
+                            <div class="w-[390px] p-6">
+                                <p>{{text.title}}</p>
+                            </div>
+                        </div>
+                    </div>
+                    </swiper-slide>
+                </swiper>        
+            </div>
+         </div>
+         <div class="flex w-4/5 mx-auto justify-between pt-3 items-center pb-10">
+             <div>
+                    <button class="w-12 h-3.5 bg-red-500 border rounded-xl"></button>
+                    <button class="w-3.5 h-3.5 bg-red-500 border rounded-xl"></button>
+                    <button class="w-3.5 h-3.5 bg-red-500 border rounded-xl"></button>
+                    <button class="w-3.5 h-3.5 bg-red-500 border rounded-xl"></button>
+            </div>
+                <div class="flex items-center">
+                    <button @click="leftSlide()" class="w-20"><img class="w-13" src="./assets/prevbtn.png" alt=""></button>
+                    <button @click="rightSlide()" class="w-20"><img class="w-13" src="./assets/nextbtn.png" alt=""></button>
+            </div>
+        </div>
+        <div class="flex w-4/5 h-60 mx-auto justify-center items-center shadow-xl shadow-gray-300">
+            <div class="flex w-[900px] justify-between items-center ">
+            <div class="w-[370px]">
+               <h1 class="font-medium text-4xl">Subscribe Now for Get Special Features!</h1>
+               <p class="font-normal pt-3">Let's subscribe with us and find the fun.</p>
+               </div>
+            <div class="w-64 h-16 bg-red-500 border rounded-xl items-center flex justify-center shadow-xl shadow-red-300">
+                <button class="text-white text-center">Subscribe Now</button>
+            </div>
+            </div>
+        </div>
+        <footer class="flex w-4/5 mx-auto pt-20 justify-between pb-20">
+            <div class="flex-col w-[600px] h-auto">
+                <div class="flex ">
+                    <img src="./assets/Vector.png" alt="">
+                    <p class="font-bold text-xl m-2 ">
+                    LaslesVPN
+                    </p>
+                </div>
+                <div class="font-medium w-[340px] pt-8">
+                    <p><span class="text-md">LaslesVPN</span>is a private virtual network that has unique features and has high security.</p>
+                </div>
+                <div class="flex w-[180px] justify-between items-center pb-5 pt-8">
+                    <div class="w-[50px] h-[50px] shadow-xl shadow-gray-300 rounded-full"><img class="w-[50px] h-[50px] bg-red-600 rounded-full" src="./assets/facebook.png" alt=""></div>
+                    <div class="w-[50px] h-[50px] shadow-xl shadow-gray-300 rounded-full"><img class="w-[50px] h-[50px] bg-red-600 rounded-full" src="./assets/instagram.png" alt=""></div>
+                    <div class="w-[50px] h-[50px] shadow-xl shadow-gray-300 rounded-full"><img class="w-[50px] h-[50px] bg-red-600 rounded-full" src="./assets/twitter.png" alt=""></div>
+                </div>
+                <div class="pt-8">
+                    <p>©2020LaslesVPN</p>
+                </div>
+            </div>
+            <div class="flex-col w-[180px] h-auto">
+                <div class="font-medium text-xl">
+                    Product
+                </div>
+                <div class="mt-4">Dowload</div>
+                <div class="mt-4">Pricing</div>
+                <div class="mt-4">Locations</div>
+                <div class="mt-4">Server</div>
+                <div class="mt-4">Countries</div>
+                <div class="mt-4">Blog</div>
+            </div>
+            <div class="flex-col w-[180px] h-auto">
+                <div class="font-medium text-xl">
+                    Engane
+                </div>
+                <div class="mt-4">LaslesVPN ? </div>
+                <div class="mt-4">FAQ</div>
+                <div class="mt-4">Tutorials</div>
+                <div class="mt-4">About Us</div>
+                <div class="mt-4">Privacy Policy</div>
+                <div class="mt-4">Terms of Service</div>
+            </div>
+            <div class="flex-col w-[180px] h-auto">
+                <div class="font-medium text-xl">
+                    Earn Money
+                </div>
+                <div class="mt-4">Affiliate</div>
+                <div class="mt-4">Become Partner</div>
+            </div>
+        </footer>
+    </div>
 </template>
-
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+    import {Swiper, SwiperSlide} from 'swiper/vue';
+    import {Autoplay, Pagination} from 'swiper'
+    // import 'swiper/css';
+    // import 'swiper/css/paginatiom';
+    import { ref } from 'vue';
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
+    component: {
+        Swiper,
+        SwiperSlide
+    },
+    setup() {
+        const swiperTextBase = ref([
+            {
+                userName: 'Viezh Robert',
+                country: 'Warsaw, Poland',
+                title: '“Wow... I am very happy to use this VPN, it turned out to be more than my expectations and so far there have been no problems. LaslesVPN always the best”.',
+                img: "requare('./assets/user1.png')",
+            },
+            {
+                userName: 'Yessica Christy',
+                country: 'Shanxi, China',
+                title: '“I like it because I like to travel far and still can connect with high speed.”.',
+                img: "requare('./assets/user2.png')",
+            },
+            {
+                userName: 'Kim Young Jou',
+                country: 'Seoul, South Korea',
+                title: '“This is very unusual for my business that currently requires a virtual private network that has high security.”.',
+                img: "requare('./assets/user3.png')",
+            }
+        ]);
+        return {modules:[ Autoplay, Pagination], swiperTextBase}
+    }
 }
 </script>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
+<style lang=""></style>
